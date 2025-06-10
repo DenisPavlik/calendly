8896311c6607c5e76d0d4bb76fc1601cfe1dcf35
Denys Pavlyk <denispavlik9@gmail.com>
2025-06-10 09:28:50 -0400

    fix: NEXT_PUBLIC_URL

--- src/libs/nylas.ts
+++ src/libs/nylas.ts
@@ -3,7 +3,6 @@ import Nylas from "nylas";
 export const nylasConfig = {
   clientId: process.env.NYLAS_CLIENT_ID,
   callbackUri: `${process.env.NEXT_PUBLIC_URL as string}/api/oauth/exchange`,
-  // callbackUri: "http://localhost:3000/api/oauth/exchange",
   apiKey: process.env.NYLAS_API_KEY,
   apiUri: process.env.NYLAS_API_URI,
 };
